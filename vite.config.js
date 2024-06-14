48eb5fb14d1c98eb3a15c6930403f21a209a6ef5
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-06-14 11:25:57 +0800

    updatte lagi

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "http://iqbalfarhan.github.io/bacaberita/",
+  base: "https://iqbalfarhan.github.io/bacaberita/",
   build: {
     outDir: "dist",
     rollupOptions: {},
