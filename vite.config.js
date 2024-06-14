8f4c1157e8f64ed6185c4f6b79ae0bb07e5ccac2
iqbal syuhada <iqbalfarhan1996@gmail.com>
2024-06-14 11:23:57 +0800

    udpate lagi

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "http://iqbalfarhan.github.io/bacaberita/",
+  build: {
+    outDir: "dist",
+    rollupOptions: {},
+  },
   plugins: [react()],
 });
